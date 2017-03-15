2c56d0864df5cb7393287a198e9f230a3d22ad40
Semen Sizov <sizov.semen@gmail.com>
2017-03-15 09:16:47 +0200

    SkillDAO implemented

--- src/main/java/dao/SkillDao.java
+++ src/main/java/dao/SkillDao.java
@@ -1,11 +1,71 @@
 package main.java.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import main.java.model.Skill;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SkillDao extends BasicDao<Long, Skill> {
 	public SkillDao(SessionFactory sessionFactory) {
 		setSession(sessionFactory);
+	}
+
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(SkillDao.class);
+
+	public Skill load(Long id){
+		Session session = getSession();
+		session.beginTransaction();
+		Skill skill = (Skill) session.createQuery("select s from Skill s where id=" + id).uniqueResult();
+		session.getTransaction().commit();
+		LOGGER.info("Skill: " + skill + " successfully loaded.");
+		session.close();
+		return skill;
+	}
+
+	public void update(Skill skill){
+		Session session = getSession();
+		session.beginTransaction();
+		Skill skillToUpdate = getById(skill.getId());
+		skillToUpdate.setName(skill.getName());
+		session.getTransaction().commit();
+		LOGGER.info("Skill : " + skill + " successfully updated.");
+		session.close();
+	}
+
+
+	public List<Skill> getAll() {
+		Session session = getSession();
+
+		session.beginTransaction();
+		List<Skill> skills = new ArrayList<>(session.createQuery("select s from Skill s").list());
+		session.getTransaction().commit();
+		session.close();
+		return skills;
+	}
+
+	@Override
+	public void save(Skill entity) {
+		Session session = getSession();
+		session.beginTransaction();
+		session.save(entity);
+		session.getTransaction().commit();
+		LOGGER.info("Skill : " + entity + " successfully save to DB.");
+		session.close();
+	}
+
+	@Override
+	public void delete(Skill entity) {
+		Session session = getSession();
+		session.beginTransaction();
+		session.delete(entity);
+		session.getTransaction().commit();
+		LOGGER.info("Skill : " + entity + " successfully deleted from DB.");
+		session.close();
 	}
 }
