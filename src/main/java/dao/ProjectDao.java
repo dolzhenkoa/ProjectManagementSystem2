3f3d50ab0f9c1e3b9a9601b86bd683286dcbfce9
Capm4 <kruhlov.aleksandr@gmail.com>
2017-03-15 12:10:11 +0300

    change CompanyDao + ProjectDao

--- src/main/java/dao/ProjectDao.java
+++ src/main/java/dao/ProjectDao.java
@@ -1,43 +1,77 @@
 package main.java.dao;
 
+import main.java.model.Company;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import main.java.model.Project;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectDao extends BasicDao<Long, Project> {
-	public ProjectDao(SessionFactory sessionFactory) {
-		setSession(sessionFactory);
-	}
+	private SessionFactory sessionFactory ;
+	private final static Logger LOGGER = LoggerFactory.getLogger(CompanyDao.class);
 
 	@Override
 	public Project getById(Long key) {
-		return super.getById(key);
+		Session session = getSession();
+		session.beginTransaction();
+		Project project = session.load(Project.class, key);
+		session.getTransaction().commit();
+		LOGGER.info("Project : " + project + " successfully loaded.");
+		return project;
 	}
 
 	@Override
-	public void save(Project entity) {
-		super.save(entity);
+	public void save(Project project) {
+		Session session = getSession();
+		session.beginTransaction();
+		session.save(project);
+		session.getTransaction().commit();
+		LOGGER.info("Project : " + project + " successfully save.");
 	}
 
 	@Override
-	public void delete(Project entity) {
-		super.delete(entity);
-	}
-
-	@Override
-	public Project get(String propertyName, Object value) {
-		return super.get(propertyName, value);
+	public void delete(Project project) {
+		Session session = getSession();
+		session.beginTransaction();
+		session.delete(project);
+		session.getTransaction().commit();
+		LOGGER.info("Project : " + project + " successfully delete.");
+		session.close();
 	}
 
 	@Override
 	public List<Project> list() {
-		return super.list();
+		Session session = getSession();
+		session.beginTransaction();
+		List<Project> projects = new ArrayList<>(session.createQuery("select p from Project  p").list());
+		session.getTransaction().commit();
+		LOGGER.info("List of Projects successfully load.");
+		session.close();
+		return projects;
 	}
 
 	@Override
-	public List<Project> getList(String propertyName, Object value) {
-		return super.getList(propertyName, value);
+	public void removeALL() {
+		Session session = getSession();
+		session.beginTransaction();
+		session.createQuery("delete from Project").executeUpdate();
+		session.getTransaction().commit();
+		LOGGER.info("Successfully remove all from Projects");
+		session.close();
+	}
+
+	public void update(Project project){
+		Session session = getSession();
+		session.beginTransaction();
+		Project projectToUpdate = getById(project.getId());
+		projectToUpdate.setName(project.getName());
+		session.getTransaction().commit();
+		LOGGER.info("Project : " + project + " successfully updated.");
+		session.close();
 	}
 }
