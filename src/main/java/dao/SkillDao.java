8d42e0e47acf96a2ca9048ad7c125e714298a98a
Semen Sizov <sizov.semen@gmail.com>
2017-03-15 12:27:51 +0200

    removeAll method implemented in SkillDAO

--- src/main/java/dao/SkillDao.java
+++ src/main/java/dao/SkillDao.java
@@ -11,61 +11,71 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SkillDao extends BasicDao<Long, Skill> {
-	public SkillDao(SessionFactory sessionFactory) {
-		setSession(sessionFactory);
-	}
+    public SkillDao(SessionFactory sessionFactory) {
+        setSession(sessionFactory);
+    }
 
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(SkillDao.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(SkillDao.class);
 
-	public Skill load(Long id){
-		Session session = getSession();
-		session.beginTransaction();
-		Skill skill = (Skill) session.createQuery("select s from Skill s where id=" + id).uniqueResult();
-		session.getTransaction().commit();
-		LOGGER.info("Skill: " + skill + " successfully loaded.");
-		session.close();
-		return skill;
-	}
+    public Skill load(Long id) {
+        Session session = getSession();
+        session.beginTransaction();
+        Skill skill = (Skill) session.createQuery("select s from Skill s where id=" + id).uniqueResult();
+        session.getTransaction().commit();
+        LOGGER.info("Skill: " + skill + " successfully loaded.");
+        session.close();
+        return skill;
+    }
 
-	public void update(Skill skill){
-		Session session = getSession();
-		session.beginTransaction();
-		Skill skillToUpdate = getById(skill.getId());
-		skillToUpdate.setName(skill.getName());
-		session.getTransaction().commit();
-		LOGGER.info("Skill : " + skill + " successfully updated.");
-		session.close();
-	}
+    public void update(Skill skill) {
+        Session session = getSession();
+        session.beginTransaction();
+        Skill skillToUpdate = getById(skill.getId());
+        skillToUpdate.setName(skill.getName());
+        session.getTransaction().commit();
+        LOGGER.info("Skill : " + skill + " successfully updated.");
+        session.close();
+    }
 
 
-	public List<Skill> getAll() {
-		Session session = getSession();
+    public List<Skill> getAll() {
+        Session session = getSession();
+        session.beginTransaction();
+        List<Skill> skills = new ArrayList<>(session.createQuery("select s from Skill s").list());
+        session.getTransaction().commit();
+        session.close();
+        return skills;
+    }
 
-		session.beginTransaction();
-		List<Skill> skills = new ArrayList<>(session.createQuery("select s from Skill s").list());
-		session.getTransaction().commit();
-		session.close();
-		return skills;
-	}
+    @Override
+    public void save(Skill entity) {
+        Session session = getSession();
+        session.beginTransaction();
+        session.save(entity);
+        session.getTransaction().commit();
+        LOGGER.info("Skill : " + entity + " successfully save to DB.");
+        session.close();
+    }
 
-	@Override
-	public void save(Skill entity) {
-		Session session = getSession();
-		session.beginTransaction();
-		session.save(entity);
-		session.getTransaction().commit();
-		LOGGER.info("Skill : " + entity + " successfully save to DB.");
-		session.close();
-	}
+    @Override
+    public void delete(Skill entity) {
+        Session session = getSession();
+        session.beginTransaction();
+        session.delete(entity);
+        session.getTransaction().commit();
+        LOGGER.info("Skill : " + entity + " successfully deleted from DB.");
+        session.close();
+    }
 
-	@Override
-	public void delete(Skill entity) {
-		Session session = getSession();
-		session.beginTransaction();
-		session.delete(entity);
-		session.getTransaction().commit();
-		LOGGER.info("Skill : " + entity + " successfully deleted from DB.");
-		session.close();
-	}
+    @Override
+    public void removeALL() {
+        Session session = getSession();
+        session.beginTransaction();
+        session.createQuery("delete from Skill");
+        session.getTransaction().commit();
+        LOGGER.info("All skills were deleted from DB.");
+        session.close();
+    }
 }
+
